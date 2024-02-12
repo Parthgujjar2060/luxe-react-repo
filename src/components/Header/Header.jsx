d30ed5fde99da96e0d53a7477ade2e4cde193315
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-02-12 02:01:40 -0500

    Header is changed with few changes like - Login,signup > Icons, Margin, 3 Layers to single layer in header.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,23 @@
 import React, { useRef } from "react";
-
+import { RiLoginCircleLine, RiUserLine } from "react-icons/ri";
 import { Container, Row, Col } from "reactstrap";
 import { Link, NavLink } from "react-router-dom";
 import "../../styles/header.css";
 
 const navLinks = [
-  
+
   {
     path: "/home",
     display: "Home",
   },
   {
-    path: "/about",
-    display: "About",
-  },
-  {
     path: "/cars",
     display: "Cars",
   },
-
+  {
+    path: "/about",
+    display: "About",
+  },
   {
     path: "/blogs",
     display: "Blog",
@@ -36,27 +35,6 @@ const Header = () => {
 
   return (
     <header className="header">
-      {/* ============ header top ============ */}
-      <div className="header__top">
-        <Container>
-          <Row>
-            <Col lg="12" md="12" sm="6">
-              <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
-                {/* Link to the login page */}
-                <Link to="/login" className=" d-flex align-items-center gap-1">
-                  <i className="ri-login-circle-line"></i> Login
-                </Link>
-
-                <Link to="/Signup" className=" d-flex align-items-center gap-1">
-                  <i className="ri-user-line"></i> Register
-                </Link>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-
-      {/* ========== main navigation =========== */}
 
       <div className="main__navbar">
         <Container>
@@ -74,20 +52,39 @@ const Header = () => {
                       navClass.isActive ? "nav__active nav__item" : "nav__item"
                     }
                     key={index}
-                  >
+                   >
                     {item.display}
                   </NavLink>
                 ))}
+                <div className="search__box" >
+                  <input type="text" placeholder="Search" />
+                  <span>
+                    <i class="ri-search-line"></i>
+                  </span>
+                </div>
+                <div className="header__top">
+                  <Container>
+                    <Row>
+                      <Col lg="12" md="12" sm="6">
+                        <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
+
+                          <Link to="/login" className=" d-flex align-items-center gap-1">
+                            <i className="ri-login-circle-line"></i> <RiLoginCircleLine /> Login
+                          </Link>
+
+                          <Link to="/Signup" className=" d-flex align-items-center gap-1">
+                            <i className="ri-user-line"></i> <RiUserLine /> Register
+                          </Link>
+                        </div>
+                      </Col>
+                    </Row>
+                  </Container>
+                </div>
               </div>
             </div>
 
             <div className="nav__right">
-              <div className="search__box">
-                <input type="text" placeholder="Search" />
-                <span>
-                  <i class="ri-search-line"></i>
-                </span>
-              </div>
+
             </div>
           </div>
         </Container>
