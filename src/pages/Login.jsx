5897de640bd658b85caa6cea7675ab744d7bc25c
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-02-21 23:08:51 -0500

    Exchange the code from feat-login with login in dev

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,35 @@
 import React, { useState } from 'react';
-import { loginUser } from '../services'
-
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { loginUser } from '../services';
+import LoginUserModel from '../models/loginUserModel';
 
 const Login = () => {
-
     const [email, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    
+
     const loginBtn = async () => {
         try {
-            
-            const user = await loginUser(email, password);
-        
-        } catch (error) {
-           
-        }
+            const userData = {
+                email: email,
+                password: password
+            }
+            const user = new LoginUserModel(userData);
+            console.log(user);
+            await loginUser(user);
 
+        } catch (error) {
+
+        }
     };
-    
 
     const handleSubmit = (e) => {
         e.preventDefault();
-    
     };
 
     return (
         <div style={styles.container}>
-            <h2 style={styles.heading}>Login</h2>
             <form style={styles.form} onSubmit={handleSubmit}>
+                <h2 style={styles.heading}>Login</h2>
                 <label style={styles.label}>
                     Username:
                     <input
@@ -51,12 +53,11 @@ const Login = () => {
                 <button onClick={loginBtn} type="submit" style={styles.button}>
                     Log In
                 </button>
-
+                <Link to="/Signup">Do signup here</Link>
             </form>
         </div>
     );
 };
-
 
 const styles = {
     container: {
@@ -64,7 +65,7 @@ const styles = {
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        height: '100vh',
+        height: '70vh',
     },
     heading: {
         fontSize: '24px',
@@ -73,27 +74,32 @@ const styles = {
     form: {
         display: 'flex',
         flexDirection: 'column',
+        alignItems: 'center',
+        border: '2px solid #ccc',
         width: '300px',
     },
     label: {
         margin: '10px 0',
         fontSize: '16px',
+        textAlign: 'left',
     },
     input: {
         padding: '8px',
         fontSize: '14px',
-        margin: '5px 0',
-        border: '1px solid #ccc',
+        margin: '5px 10px',
+        border: '2px solid #ccc',
         borderRadius: '4px',
     },
     button: {
-        backgroundColor: '#4CAF50',
-        color: 'white',
+        backgroundColor: '#000d6b',
+        color: '#ffffff',
         padding: '10px',
         fontSize: '16px',
         border: 'none',
-        borderRadius: '4px',
+        borderRadius: '15px',
         cursor: 'pointer',
+        marginTop: '10px',
+        marginBottom: '10px',
     },
 };
 
