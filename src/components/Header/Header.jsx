dcadba839a360a8744033142d1285bb27ce2fb8f
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-04-20 23:46:13 -0400

    Header shows hamburger menu now...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState, useEffect, useId } from "react";
-import { RiLoginCircleLine, RiUserLine } from "react-icons/ri";
+import React, { useRef, useState, useEffect } from "react";
+import { RiMenuLine, RiLoginCircleLine, RiUserLine } from "react-icons/ri";
 import { Container, Row, Col } from "reactstrap";
 import { Link, NavLink } from "react-router-dom";
 import "../../styles/header.css";
@@ -34,8 +34,6 @@ const Header = ({ loginResponse }) => {
   const [sessionToken, setSessionToken] = useState(null);
   const [showMenu, setShowMenu] = useState(false);
 
-
-
   const fetchUserData = async () => {
     try {
       const userData = await getuserData(sessionToken);
@@ -47,30 +45,22 @@ const Header = ({ loginResponse }) => {
   };
 
   useEffect(() => {
-
     // get the token from local storage
     const token = localStorage.getItem("token");
     if (token) {
-      try { 
+      try {
         fetchUserData();
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
       setSessionToken(token);
     }
-
-    // set the token in the state
-    setSessionToken(token);
-
-
-  }, [sessionToken]);
-
+  }, []);
 
   const logOut = () => {
     localStorage.removeItem("token");
     window.location.href = "/home";
   };
-
 
   return (
     <header className="header">
@@ -78,9 +68,8 @@ const Header = ({ loginResponse }) => {
         <Container>
           <div className="navigation__wrapper d-flex align-items-center justify-content-between">
             <span className="mobile__menu" onClick={() => setShowMenu(!showMenu)}>
-              <i className="ri-menu-line"></i>
+              <RiMenuLine />
             </span>
-
             <div className={`menu ${showMenu ? 'show' : 'hide'}`}>
               {navLinks.map((item, index) => (
                 <NavLink
@@ -100,39 +89,29 @@ const Header = ({ loginResponse }) => {
                   <i className="ri-search-line"></i>
                 </span>
               </div>
+
               <div className="header__top">
                 <Container>
                   <Row>
                     <Col lg="12" md="12" sm="6">
                       <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
-                        {
-
-                          console.log("userId", userId)
-                        }
-                        {sessionToken ?
+                        {sessionToken ? (
                           <div className="responseLogin">
                             <p>Hello, {userId}</p>
-                          </div> : (
-                            <div style={{
-                              display: "flex",
-                              gap: "1rem",
-                            }}>
-                              <Link to="/login" className="login_left d-flex align-items-center gap-1">
+                            <Link className="login_left d-flex align-items-center gap-1" onClick={logOut}>
+                              <i className="ri-user-line"></i> <RiUserLine /> Logout
+                            </Link>
+                          </div>
+                        ) : (
+                          <div style={{ display: "flex", gap: "1rem" }}>
+                            <Link to="/login" className="login_left d-flex align-items-center gap-1">
                               <i className="ri-login-circle-line"></i> <RiLoginCircleLine /> Login
                             </Link>
-    
                             <Link to="/Signup" className="signup_left d-flex align-items-center gap-1">
                               <i className="ri-user-line"></i> <RiUserLine /> Register
                             </Link>
-                            </div>
-                          )
-                        }
-                        
-                        {
-                         sessionToken ? <Link className="login_left d-flex align-items-center gap-1" onClick={logOut}>
-                            <i className="ri-user-line"></i> <RiUserLine /> Logout
-                          </Link> : null
-                        }
+                          </div>
+                        )}
                       </div>
                     </Col>
                   </Row>
